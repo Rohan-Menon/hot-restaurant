c85dd7efbff3c4ef1c5ec496de67c9de8bb934e9
thaiscmky <22459337+thaiscmky@users.noreply.github.com>
2018-04-24 21:16:44 -0500

    Re-adds tables and reserve routes to files

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,7 +8,7 @@ require('bootstrap/dist/css/bootstrap.css');
 module.exports = function(app, reservations) {
     console.log('export worked html');
 
-   
+
     app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname, '../public/tables.html'));
 
@@ -20,4 +20,12 @@ module.exports = function(app, reservations) {
     // app.get('/reserve', function(req, res) {
     //     res.sendFile(path.join(__dirname, '../public/survey.html'));
     // });
+
+    app.get('/tables', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/tables.html'));
+    });
+
+    app.get('/reserve', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/reservation.html'));
+    });
 };

7b7249109ef456a9043fb80f93585068b68b2323
Brant Campodonico <brantcampodonico@Brants-MacBook-Pro.local>
2018-04-24 20:04:15 -0500

    created user api

---
+++ api/api.js
@@ -0,0 +1,14 @@
+
+
+var users = [];
+
+var user = {
+    name:$('#name').val().trim(),
+    phone:$('#phone').val().trim(),
+    email:$('#email').val().trim(),
+    id:$('#id').val().trim(),
+};
+
+users.push(user);
+
+

6a0bcf81f36cca7c671ac2a3cd73b0a7b4721b68
thaiscmky <22459337+thaiscmky@users.noreply.github.com>
2018-04-24 20:44:53 -0500

    Adds htmlRoutes logic slacked by Rohan

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,29 @@
+// dependencies
+var path = require('path');
+
+var jquery = require('jquery');
+
+// HTML routes
+module.exports = function(app, reservations) {
+    console.log('export worked html');
+
+
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, '../public/tables.html'));
+
+
+        for(var i = 0; reservations.length; i++){
+
+            jquery("#reservations").append("<h2>"+ i + " | "+reservations[i].id + "</h2>");
+
+        }
+
+    });
+    // app.get('/tables', function(req, res) {
+    //     res.sendFile(path.join(__dirname, '../public/survey.html'));
+    // });
+
+    // app.get('/reserve', function(req, res) {
+    //     res.sendFile(path.join(__dirname, '../public/survey.html'));
+    // });
+};
